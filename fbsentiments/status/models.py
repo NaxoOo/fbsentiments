1bffe00c59574d8ae2e1f066837afc3abec5045f
Ignacio Astudillo <ieastudi@uc.cl>
2016-04-30 21:41:53 -0300

    Agregado codigo de connect, falta corregir paquete de facebook.

--- fbsentiments/status/models.py
+++ fbsentiments/status/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+	profile_id = models.CharField(max_length=200)
+
+	#def was_published_recently(self):
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class App(models.Model):
+	app_id = models.CharField(max_length=200)
+	app_secret = models.CharField(max_length=200)
--- fbsentiments/status/views.py
+++ fbsentiments/status/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+#import facebook
+import urllib
+from urllib.parse import urlparse
+import subprocess
+import warnings
+
+import oauth2 as oauth
+
+
+def connect(request):
+	consumer_key = '279983135533947'
+	consumer_secret = '9ff237e023f95b524274adef3af1ea21'
+
+	redirect_uri = 'https://fbsentiments.azurewebsites.net/status/authorize'
+
+	request_token_url = 'https://www.facebook.com/dialog/oauth?client_id=' + consumer_key
+	+ '&redirect_uri=' + redirect_uri
+	+ '&scope=user_posts'
+	access_token_url = 'https://graph.facebook.com/v2.3/oauth/access_token?client_id=' + consumer_key
+	+ '&redirect_uri=' + redirect_uri
+	+ '&client_secret=' + consumer_secret
+	+ '&code='#{code-parameter}
+	#authorize_url = 'https://api.twitter.com/oauth/authorize'
+	#Authorization URL: https://www.facebook.com/dialog/oauth (the scopes that can be specified with this URL, can be found here)
+
+	consumer = oauth.Consumer(consumer_key, consumer_secret)
+	client = oauth.Client(consumer)
+
+	# Step 1: Get a request token. This is a temporary token that is used for 
+	# having the user authorize an access token and to sign the request to obtain 
+	# said access token.
+
+	resp, content = client.request(request_token_url, "GET")
+	#OJO: revisar error code (cuando sepa el formato)
+	#https://developers.facebook.com/docs/graph-api/using-graph-api/#errors
+	#if resp['status'] != '200':
+	    #raise Exception("Invalid response %s." % resp['status'])
+
+	request_token = dict(urlparse.parse_qsl(content))
+
+	#print "Request Token:"
+	#print "    - oauth_token        = %s" % request_token['oauth_token'] #Ver nombre en documentación de facebook.
+	#print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
+	#print 
+
+	return HttpResponse("Hello, world. You're at the polls index.")
+    #https://www.facebook.com/dialog/oauth?client_id=279983135533947&redirect_uri=https://fbsentiments.azurewebsites.net/status/authorize&scope=user_posts
+
+def authorize(request):
+	return HttpResponse("Holi, esto falta.")

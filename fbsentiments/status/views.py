b676ec7060d3ef76af83023571ef67facc0460da
Ignacio Astudillo <ieastudi@uc.cl>
2016-05-02 23:19:15 -0300

    Corregida library de facebook.

--- fbsentiments/status/views.py
+++ fbsentiments/status/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-#import facebook
+import facebook
 import urllib
 from urllib.parse import urlparse
 import subprocess
